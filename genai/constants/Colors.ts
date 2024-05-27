73cd7a0d432b873826afdef4080d2957123cef22
KBismark <bismarkkwabenayamoah@gmail.com>
2024-05-27 09:08:09 +0000

    Set up theming (dark and light modes) into app

--- genai/constants/Colors.ts
+++ genai/constants/Colors.ts
@@ -1,13 +1,29 @@
+// import { type ReactNode, createContext, useContext, useState, useMemo } from "react";
+
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
-export default {
+
+
+const Theme = {
   light: {
     text: '#000',
     background: '#fff',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
+    colors:{
+      primary: '#3858AD',
+      white: '#FFFFFF',
+      black: '#000000',
+      text: '#242424',
+      searchBar: '#F2F2F2',
+      fadedBlack: '#333333',
+      neutral: '#EFF0F3',
+      background: '#E2E2E2',
+      danger: '#E23131'
+    },
+
   },
   dark: {
     text: '#fff',
@@ -15,5 +31,21 @@ export default {
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
+    // Update these color values if you wish to support dark mode
+    colors:{
+      primary: '#3858AD',
+      white: '#FFFFFF',
+      black: '#000000',
+      text: '#242424',
+      searchBar: '#F2F2F2',
+      fadedBlack: '#333333',
+      neutral: '#EFF0F3',
+      background: '#E2E2E2',
+      danger: '#E23131'
+    },
   },
 };
+
+
+
+export default Theme;
---
+++ genai/constants/Theme.tsx
@@ -0,0 +1,35 @@
+import { type ReactNode, createContext, useContext, useState, useMemo } from "react";
+import Theme from "./Colors";
+/**
+ * 
+ * A simple setup to support both light and dark modes 
+ * 
+ */
+type Modes = 'light'|'dark';
+const ThemeContext = createContext(Theme.light);
+const Provider = ThemeContext.Provider;
+// A method to make it easy to implement dark mode feature if you wish to
+export const useTheme = ()=>{
+  return useContext(ThemeContext)
+}
+// Call this method to change the theme of the app. (Don't forget to update the dark colors above)
+export const setThemeMode = (mode: Modes)=>{
+  switch (mode) {
+    case 'dark':
+      break;
+    default:
+      // fallback to light mode if 'light' or any other value was provided
+      mode = 'light'
+      break;
+  }
+  themeSetter(Theme[mode] as any);
+}
+let themeSetter: React.Dispatch<React.SetStateAction<typeof Theme['light']>> = (()=>{}) as any;
+// The theming provider. Must be the parent component of your main App component.
+export const AppThemeProvider = ({children}:{children:ReactNode|React.JSX.Element})=>{
+  const [theme,setTheme] = useState(Theme.light);
+  themeSetter = setTheme;// Make state setter publicly accessible in this module
+  return (
+    <Provider value={theme}>{children}</Provider>
+  )
+}
